7b7659dc0f32ef528f8f1772168a7ac548efda5a
celeste <celeste.tiano@etu.toulouse-inp.fr>
2024-12-27 00:21:59 +0100

    branchage

--- preprocessing.py
+++ preprocessing.py
@@ -1,28 +1,50 @@
 import csv
 import tensorflow as tf
 
-
 class Table () : 
     
     def __init__ (self) :
         self.table = {"<pad>" : 0, "<eos>" : 1, "<sos>" : 2}
         self.vocab_size = 3
         self.reverse_table = {0:"<pad>",1:"<eos>", 2:"<sos>"}
+        self.frequency = {}
         
     def get_and_add (self, character) :
         if character not in self.table :
             self.table[character] = self.vocab_size
             self.vocab_size += 1
+            self.frequency[character] = 1
             
             self.reverse_table[self.vocab_size - 1] = character
             
             return self.vocab_size - 1
 
         else :
+            self.frequency[character] += 1
             return self.table[character]
         
     def number_to_character (self, number) :
         return self.reverse_table[int(number)]
+    
+    def crop (self, min_occurence) : 
+        new_table = {"<pad>" : 0, "<eos>" : 1, "<sos>" : 2}
+        new_reverse_table = {0:"<pad>",1:"<eos>", 2:"<sos>"}
+        new_vocab_size = 3
+        new_frequency = {}
+        
+        for key in self.table :
+            if key == "<pad>" or key == "<eos>" or key == "<sos>" :
+                continue
+            if self.frequency[key] >= min_occurence :
+                new_table[key] = new_vocab_size
+                new_reverse_table[new_vocab_size] = key
+                new_vocab_size += 1
+                new_frequency[key] = self.frequency[key]
+        
+        self.table = new_table
+        self.reverse_table = new_reverse_table
+        self.vocab_size = new_vocab_size
+        self.frequency = new_frequency
         
 
 def load_data () :
@@ -54,30 +76,25 @@ def preprocessing (text, table) :
         finaly we change our character to numbers
 
     """
+    splited_text = text.split(" ")
+    splited_text = [word.lower() for word in splited_text]  # on met tout en minuscule
     
     new_text = ""
     vector = []
     idx = 0
-    while idx < len(text) and len(text) != 0 :
-        char = text[idx]
-        if char == "\n" :
+    while idx < len(splited_text) and len(splited_text) != 0 :
+        word = splited_text[idx]
+        if word == "\n" :
             idx +=1 
             continue 
         
-        if char == "h" and text[idx:idx+4] == "http" :
-            while idx < len(text) and text[idx] != " " :
-                idx += 1
-            continue
-        
-        if char.isupper() :
-            new_text += char.lower()
-            vector.append(character_to_number(char.lower(),table))
+        elif "http" in word :
             idx += 1
+            continue
 
-        
         else : 
-            new_text += char
-            vector.append(character_to_number(char,table))
+            new_text += word
+            vector.append(character_to_number(word,table))
             idx += 1
 
             
@@ -92,7 +109,7 @@ def padding (vector, max_size) :
     if len(vector) < max_size :
         return  [2]+vector+[1]+[0]*(max_size-len(vector)-2) # on ajoute un <sos> et un <eos> à la fin du tweet
     
-def getdataset (token_type = "character", max_size=280) :
+def getdataset () :
     """
         token_type : str, type de tokenisation à utiliser; character ou word, ou token
     """
@@ -100,17 +117,24 @@ def getdataset (token_type = "character", max_size=280) :
     tweets = load_data()
     data = []
     table = Table()
+    max_size = 0
     
-    if token_type == "character" :
-        for idx, tweet in enumerate(tweets) :
-            vec = preprocessing(tweet,table)
-            padded_vec = padding(vec,max_size)
-            if padded_vec == None :
-                continue
-            tensor =padded_vec
-            data.append(tensor)
+    for tweet in tweets :
+        vec = preprocessing(tweet,table)
+        if len(vec) > max_size :
+            max_size = len(vec)
+    max_size = max_size + 2 # on ajoute un <sos> et un <eos> à la fin du tweet
+    
+    for idx, tweet in enumerate(tweets) :
+        vec = preprocessing(tweet,table)
+        padded_vec = padding(vec,max_size)
+        if padded_vec == None :
+            continue
+        tensor =padded_vec
+        data.append(tensor)
             
-    return data,table
+    print("shocolat")
+    return data,table, max_size
             
         
 def to_text (vector,table) :
