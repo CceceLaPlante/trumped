bfd911e3f84d025ea554c2eec7d6ae16a9423280
celeste <celeste.tiano@etu.toulouse-inp.fr>
2024-12-27 14:32:40 +0100

    je pense que c'est peine perdue

--- network.py
+++ network.py
@@ -8,18 +8,15 @@ num_heads = 2  # Nombre de têtes d'attention
 
 class Net (layers.Layer) :
 
-    def __init__(self, maxlen, vocab_size, embed_dim, num_heads,num_blocs=8) :
+    def __init__(self, maxlen, vocab_size, embed_dim, num_heads,num_blocs=8,hidden_layer=64) :
         super().__init__()
         self.embeddings = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
         
         self.attention_blocks = [TransformerBlock(embed_dim, num_heads) for _ in range(num_blocs)]
-        self.post_processing = layers.Dense(256,activation="relu")
+        self.post_processing = layers.Dense(hidden_layer,activation="relu")
         
         self.softmax = layers.Dense(vocab_size,activation="softmax")
-        #dropout
-        self.dropout = layers.Dropout
-        self.dropout_rate = 0.1
-        self.dropout = self.dropout(self.dropout_rate)
+ 
         
         
         
@@ -29,13 +26,11 @@ class Net (layers.Layer) :
         for block in self.attention_blocks :
             x = block(x)
         
-        x = self.dropout(x)
-
         x = self.post_processing(x)
         return self.softmax(x)
     
 
-def make_model (maxlen,vocab_size, embed_dim, num_heads,num_blocs=8) :
+def make_model (maxlen,vocab_size, embed_dim, num_heads,num_blocs=8,hidden_layer=64) :
     inputs = layers.Input(shape=(maxlen,))
     net = Net(maxlen, vocab_size, embed_dim, num_heads,num_blocs)(inputs)
     
